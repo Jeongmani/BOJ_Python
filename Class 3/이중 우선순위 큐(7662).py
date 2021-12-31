4195786f4993cac666a8e39a278186caeb5e5d21
Jeongmin Lee <jeongmin7244@naver.com>
2021-12-31 21:27:53 +0900

    Time over error

---
+++ Class 3/이중 우선순위 큐(7662).py
@@ -0,0 +1,23 @@
+import heapq
+import sys
+array_min=[]
+array_max=[]
+T=int(sys.stdin.readline().rstrip())
+for _ in range(T):
+    k=int(sys.stdin.readline().rstrip())
+    for _ in range(k):
+        command=list(map(str,sys.stdin.readline().split()))
+        if command[0]=='I':
+            heapq.heappush(array_min,int(command[1]))
+            heapq.heappush(array_max,-int(command[1]))
+        if command[0]=='D':
+            if len(array_min)==0 or len(array_max) ==0:
+                print('EMPTY')
+            else:
+                if command[1]=='1':
+                    a=-heapq.heappop(array_max)
+                    array_min.remove(a)
+                elif command[1]=='-1':
+                    a=heapq.heappop(array_min)
+                    array_max.remove(-a)
+    print(-heapq.heappop(array_max),heapq.heappop(array_min))
