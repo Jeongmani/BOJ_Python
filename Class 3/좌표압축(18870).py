a75704d3489b2da9e8b4f66e74b7902e8eb4798f
Jeongmin Lee <jeongmin7244@naver.com>
2022-01-06 16:37:05 +0900

    Update

--- Class 3/좌표압축(18870).py
+++ Class 3/좌표압축(18870).py
@@ -1,20 +1,7 @@
-import heapq
-import sys
-rank=[]
-ranking=0
+import sys                                         #처음에는 우선순위 큐를 이용해 제일 작은 값부터 리스트를 돌며 랭킹을 주는 식으로 코딩을 진행했지만 문제에서 시간초과오류가 발생
 n=int(sys.stdin.readline().rstrip())
 N=list(map(int,sys.stdin.readline().split()))
-N_set=set(N)
-for i in N_set:
-    heapq.heappush(rank,i)
-while True:
-    if len(rank)==0:
-        break
-    else:
-        temp=heapq.heappop(rank)
-    for i in range(n):
-        if N[i]==temp:
-            N[i]=ranking 
-    ranking+=1
-for i in N:
-    print(i,end=' ')   
+N_set=sorted(list(set(N)))                         #set 자료형을 이용해 중복이 되는 수를 줄여주고
+dic={N_set[i] : i for i in range(len(N_set))}      #사전 자료형을 통해 수 별로 key값을 부여해 
+for i in N:                                        #N에서 숫자별로 key값을 출력함                
+    print(dic[i],end=' ')   
