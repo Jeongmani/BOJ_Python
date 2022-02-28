095b95e35fa033519039a1445664e0ab52970fc0
이정민 <jeongmin7244@naver.com>
2022-02-28 14:29:47 +0900

    Update

---
+++ BOJ/Pratice/저울(2437).py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+weight=list(map(int,input().split()))
+weight.sort()
+target=1
+
+for i in weight:
+    if target<i:
+        break
+    target+=i
+print(target)
